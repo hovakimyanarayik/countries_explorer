dae3efe0be3499607fbc2906c583ab70bc47ede2
hovakimyanarayik <hovaro1907@gmail.com>
2022-11-17 10:33:09 +0400

    adding to github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import {Routes, Route } from 'react-router-dom'
+import axios from 'axios';
+import Controls from './components/Controls';
+import Header from './components/Header';
+import Main from './components/Main';
+
+import HomePage from './pages/HomePage';
+import Details from './pages/Details';
+import NotFound from './pages/NotFound';
+
+
 
 function App() {
+  const [countries, setCountries] = useState([])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Main>
+      <Routes>
+        <Route path='/' element={<HomePage countries={countries} setCountries={setCountries} />} />
+        <Route path='/country/:name' element={<Details />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Main>
+    </>
   );
 }
 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios';
+
+import { ALL_COUNTRIES } from '../config';
+import List from '../components/List';
+import Card from '../components/Card';
+import Controls from '../components/Controls';
+
+const HomePage = ({setCountries, countries}) => {
+    const [filteredCountries, setFilteredCountries] = useState(countries)
+    const navigate = useNavigate()
+    useEffect(() => {
+        if(!countries.length)
+      axios.get(ALL_COUNTRIES).then(({data}) => setCountries(data));
+    }, [])
+
+    useEffect(() => {
+        handleSearch()
+    }, [countries])
+
+    const handleSearch = (search, region) => {
+        let data = [...countries];
+
+        if(region) {
+            data = data.filter(c => c.region.includes(region))
+        }
+        if(search) {
+            data = data.filter(c => c.name.toLowerCase().includes(search.toLowerCase()))
+        }
+
+        setFilteredCountries(data)
+    }
+    return ( 
+        <>
+            <Controls onSearch={handleSearch} />
+            <List>
+                {filteredCountries.map(c => {
+                const countryInfo = {
+                    img: c.flags.png,
+                    name: c.name,
+                    info: [
+                    {
+                        title: 'Population',
+                        description: c.population.toLocaleString()
+                    },
+                    {
+                        title: 'Region',
+                        description: c.region
+                    },
+                    {
+                        title: 'Capital',
+                        description: c.capital
+                    }
+                    ]
+                };
+
+                return (
+                    <Card key={c.name} onClick={() => navigate(`/country/${c.name}`) } {...countryInfo} />
+                )
+                })}
+            </List>
+        </>
+     );
+}
+ 
+export default HomePage;
